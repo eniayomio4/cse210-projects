a79c868add3ace60dedab96f6caca8ed0e0ff696
eniayomio4 <eniayomiojulari@gmail.com>
2025-12-06 13:28:13 +0100

    Completed Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("blue", 4, 6));
+        shapes.Add(new Circle("green", 3));
+        shapes.Add(new Square("yellow", 2.5));
+        shapes.Add(new Circle("purple", 1.5));
+
+        Console.WriteLine("Shape Areas:\n");
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has area {area:F2}");
+        }
     }
 }
